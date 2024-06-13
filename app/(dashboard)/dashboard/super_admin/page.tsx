e9a7fe087817ebfc72445f96c761fb9d8757990e
saifulaija <saifulaija2023@gmail.com>
2024-06-13 16:13:27 +0600

    logo adn favicon done

--- app/(dashboard)/dashboard/super_admin/page.tsx
+++ app/(dashboard)/dashboard/super_admin/page.tsx
@@ -1,78 +1,165 @@
+
 'use client'
 
-import AreaChart from '@/components/shared/AreaChart/AreaChart';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react';
+import { Bar, Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend, Title, TimeScale } from 'chart.js';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useGetMetaQuery } from '@/redux/api/metaDataApi';
-import { CheckCheck, CircleEllipsis, DollarSign, NotebookPen, Users, View } from 'lucide-react'
-import React from 'react'
+import { CheckCheck, CircleEllipsis, NotebookPen, User, View } from 'lucide-react';
+import 'chartjs-adapter-date-fns';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend, Title, TimeScale);
 
 const SuperAdminDashboardPage = () => {
   const { data, isLoading } = useGetMetaQuery(undefined);
-  return (
-    <div>
+  const barChartData = {
+    labels: data?.barChartData?.map((item:any) => new Date(item.day)) || [],
+    datasets: [
+      {
+        label: 'Counts by Date',
+        data: data?.barChartData?.map((item:any) => item.count) || [],
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
 
-<div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-          <Card x-chunk="dashboard-01-chunk-0">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-              Total Blogs
-              </CardTitle>
-              
-              <NotebookPen className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold"> {data?.blogCount}</div>
-              <p className="text-xs text-muted-foreground">
-                +20.1% from last month
-              </p>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-01-chunk-1">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Approve Blogs
-              </CardTitle>
-              
-              <CheckCheck className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{data?.approvedBlogCount}</div>
-              <p className="text-xs text-muted-foreground">
-                +180.1% from last month
-              </p>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-01-chunk-2">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Pending Blogs</CardTitle>
-              {/* <CreditCard className="h-4 w-4 text-muted-foreground" /> */}
-              <CircleEllipsis className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{data?.pendingBlogCount}</div>
-              <p className="text-xs text-muted-foreground">
-                +19% from last month
-              </p>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-01-chunk-3">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Total Views</CardTitle>
-              {/* <Activity className="h-4 w-4 text-muted-foreground" /> */}
-              <View className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold"> {data?.totalViews}</div>
-              <p className="text-xs text-muted-foreground">
-                +201 since last hour
-              </p>
-            </CardContent>
-          </Card>
+  const barChartOptions = {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+        },
+        title: {
+          display: true,
+          text: 'Date',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Count',
+        },
+      },
+    },
+    responsive: true,
+  } as const;
+  
+
+  const pieChartData = {
+    labels: [
+      'Admin Count', 
+      'Approved Blog Count', 
+      'Blog Count', 
+      'Blogger Count', 
+      'Cancel Blog Count', 
+      'Comment Count', 
+      'Like Count', 
+      'Moderator Count', 
+      'Pending Blog Count'
+    ],
+    datasets: [
+      {
+        label: 'Counts',
+        data: [
+          data?.adminCount, 
+          data?.approvedBlogCount, 
+          data?.blogCount, 
+          data?.bloggerCount, 
+          data?.cancelBlogCount, 
+          data?.commentCount, 
+          data?.likeCount, 
+          data?.moderatorCount, 
+          data?.pendingBlogCount
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(199, 199, 199, 0.6)',
+          'rgba(83, 102, 255, 0.6)',
+          'rgba(155, 99, 132, 0.6)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(199, 199, 199, 1)',
+          'rgba(83, 102, 255, 1)',
+          'rgba(155, 99, 132, 1)'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="p-4">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Blogs</CardTitle>
+            <NotebookPen className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data?.blogCount}</div>
+           
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Approved Blogs</CardTitle>
+            <CheckCheck className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data?.approvedBlogCount}</div>
+           
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Pending Blogs</CardTitle>
+            <CircleEllipsis className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data?.pendingBlogCount}</div>
+           
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Views</CardTitle>
+            <User className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data?.userCount}</div>
+          
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="mt-10 flex container flex-col md:flex-row md:gap-10">
+        <div className="flex-1 mb-10 md:mb-0">
+          <h2 className="text-xl font-semibold mb-3 text-center">Bar Chart</h2>
+          <Bar data={barChartData} options={barChartOptions} />
         </div>
 
-<AreaChart/>
+        <div className="flex-1 max-w-[450px]">
+          <h2 className="text-xl font-semibold mb-3 text-center">Pie Chart</h2>
+          <Pie data={pieChartData} options={{ responsive: true }} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SuperAdminDashboardPage
+export default SuperAdminDashboardPage;
--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,6 @@
 const assets = {
     images: {
-     logo:require("./assets/icons/icon.svg"),
+     logo:require("./assets/icons/icon.png"),
      banner:require("./assets/images/banner.png"),
      login:require("./assets/images/blog-login.jpeg"),
      register:require("./assets/images/blog-register.jpg"),
